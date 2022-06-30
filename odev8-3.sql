f998a4f22e6647fc0bf7f9f983f5acf03197e7c4
RecepOzdes <recepozdes@gmail.com>
2022-06-30 17:11:03 +0300

    Create odev8-3.sql

---
+++ odev8-3.sql
@@ -0,0 +1,20 @@
+UPDATE employee
+SET name = 'Recep'
+WHERE id = '1';
+
+UPDATE employee
+SET birthday = '1968-12-12'
+WHERE id = '7';
+
+UPDATE employee
+SET name = 'Ali',
+	email = 'ali@ali.com'
+WHERE id = '15';
+
+UPDATE employee
+SET name = 'Esra'
+WHERE id = '42';
+
+UPDATE employee
+SET name = 'Gökçe'
+WHERE id = '13';
