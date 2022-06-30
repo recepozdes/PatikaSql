bb675a2b721dbae64de282687db9717a113d446a
RecepOzdes <recepozdes@gmail.com>
2022-06-30 17:14:25 +0300

    Create odev8-4.sql

---
+++ odev8-4.sql
@@ -0,0 +1,14 @@
+DELETE FROM employee
+WHERE id= '15';
+
+DELETE FROM employee
+WHERE name= 'Recep';
+
+DELETE FROM employee
+WHERE id= '22';
+
+DELETE FROM employee
+WHERE id= '33';
+
+DELETE FROM employee
+WHERE id= '10';
